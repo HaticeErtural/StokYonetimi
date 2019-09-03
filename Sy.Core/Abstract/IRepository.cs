52420c530c85fcd253207ce71f7bc5a3dc06a7d6
HaticeErtural <haticeertural@outlook.com>
2019-09-03 15:13:25 +0300

    Crud işlemleri

---
+++ Sy.Core/Abstract/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstract
+{
+
+    public interface  IRepository <TEntity, TKey> where  TEntity : IEntity<TKey>
+    {
+        TEntity GetById(TKey id);
+        TKey Insert(TEntity entity);
+        int UPdate(TEntity entity);
+        int Delete(TEntity entity);
+        int Save();
+        IQueryable<TEntity> Query(Expression<Func<TEntity,bool>> predicate =null);//geriye entity tipinde değer döndürüyor,null default olarak atanıyor
+
+
+
+
+    }
+}
--- Sy.Forms/Auth/RegisterForm.cs
+++ Sy.Forms/Auth/RegisterForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Sy.Forms.Auth
 {
     public partial class RegisterForm : Form
